156ed5353f37f5a0436e35a47facbdc6f9f187ec
Gadde <SurendraKumar_Gadde@McAfee.com>
2021-12-18 18:11:00 +0530

    Modified to add collection

--- src/main/java/com/linkedin/collections/Application.java
+++ src/main/java/com/linkedin/collections/Application.java
@@ -1,18 +1,27 @@
 package com.linkedin.collections;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 public class Application {
 
 	public static void main(String[] args) {
 	
 		Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
 		Room manchester = new Room("Manchester", "Suite", 5, 250.0);
-
-		double total = getPotentialRevenue(cambridge, manchester);
+        Room room2 =  new Room("Room2", "Suite", 5, 290.0);
+        Collection<Room> rooms = List.of(cambridge,manchester, room2);
+		double total = getPotentialRevenue(rooms);
 		System.out.println(total);
 	}
 	
-	private static double getPotentialRevenue(Room room1, Room room2) {
-		return room1.getRate() + room2.getRate();
+	private static double getPotentialRevenue(Collection<Room> rooms) {
+		//return room1.getRate() + room2.getRate();
+		double total = rooms.stream()
+				.mapToDouble(room-> room.getRate()).sum();
+		return total;
+				
 	}
 
 }
